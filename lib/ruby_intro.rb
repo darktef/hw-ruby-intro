7baed12be5bd11a7b185a3523c6f2f6cfcf5fb67
darktef <darktef@gmail.com>
2016-07-03 20:31:48 -0500

    1st homework completed

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,47 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+	arr.reduce(0,:+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum(arr.max(2))
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.permutation(2).to_a.map{|x| sum(x)}.include? n
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s =~ /^[^aeiou\W]/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s =~ /^[0-1]{1,100}$/
+    return true if s.to_i(2)%4 == 0
+  end
+  false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    '$' + '%.2f' % @price
+  end
+
 end
